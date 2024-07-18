01c4949b53088cd1c4cf13b2af452b86c58f8217
Saif <muhammadsaif713@gmail.com>
2024-07-19 02:02:06 +0400

    fix: rename api key

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,12 +4,8 @@ import { validations } from "@/lib/data";
 import { FormData, fieldType } from "@/lib/type";
 import { z } from "zod";
 import { GoogleGenerativeAI, FunctionDeclarationSchemaType } from '@google/generative-ai';
-import dotenv from 'dotenv';
-import path from 'path';
 
-dotenv.config({ path: path.resolve(process.cwd(), '.env.local') });
-
-const genAI = new GoogleGenerativeAI(process.env.GOOGLE_AI_API_KEY!);
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
